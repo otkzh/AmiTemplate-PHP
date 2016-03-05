9d58ab977e976fc9075ef0d12ec9724fb296cb8f
amishiro <amishiro@gmail.com>
2016-03-06 08:32:06 +0900

    最新版をdest

--- dest/inc/sample.php
+++ dest/inc/sample.php
@@ -1,8 +1,12 @@
 <article class="wrapper">
 <h1 class="page-title"><?php echo $title ?></h1>
 <section>
-<h1 class="section-title">リンク関連</h1> <a href="#">テキストリンク</a> <br> <a href="#" target="_blank" rel="nofollow">外部リンク</a> <a href="#" class="bt">テキストリンク</a>
-<p>画像リンク</p> <a href="#"><img src="<?php echo $base_url ?>/img/dummy/img.png" alt="ダミー" width="200" height="120" /></a>
+<h1 class="section-title">リンク関連</h1>
+<div><a href="#">テキストリンク</a></div>
+<div><a href="#" target="_blank" rel="nofollow">外部リンク</a></div>
+<div><a href="#" class="a-text">かっこいいテキストリンク</a></div>
+<div><a href="#" class="bt">テキストリンク</a></div>
+<h2 class="large-title">画像リンク</h2> <a href="#"><img src="<?php echo $base_url ?>/img/dummy/img.png" alt="ダミー" ></a>
 <hr>
 <p>sns</p>
 <?php include ($inc_path."/inc/sns.php") ?> </section>
@@ -15,7 +19,7 @@
 <h2 class="small-title">.small-title 小さめ見だし</h2> </section>
 <section>
 <h1 class="section-title">文章 p：タグとか</h1>
-<p>pタグ：テキストダミー漢字ABCabcqpテキスト<strong>strong:テキストダミー漢字ABCabcqp</strong>ダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqp</p>
+<p>pタグ：テキストダミー漢字ABCabcqpテキスト<strong>strong:テキストダミー漢字ABCabcqp</strong>ダミー漢字<a href="#" class="a-text">ABCabcqpテキストダミー漢字</a>ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqp</p>
 <p>pタグ：テキストダミー漢字ABCabcqpテキスト<a href="#">strong:テキストダミー漢字ABCabcqp</a>ダミー漢字ABCabcqpテキスト<a href="#" target="_blank" rel="nofollow">ダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabc</a>qpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqpテキストダミー漢字ABCabcqp</p>
 <p class="cap">※注釈ダミーダミーダミーダミーダミーダミーダミーダミー</p>
 <hr>
@@ -37,10 +41,20 @@
 </section>
 <section>
 <h1 class="section-title">フォーム（input）</h1>
-<form action="xxx.php" method="post"><label>メール（type="email"）:<input type="email" name="email" placeholder="info@amiten.co.jp" ></label><input type="submit" value="送信"></form>
-<form action="xxx.php" method="post"><label>検索（type="search"）:<input type="search" name="search" placeholder="ワード入力"></label><input type="submit" value="送信"></form>
-<form action="xxx.php" method="post"><label>電話（type="telephone"）:<input type="tel" name="tel" placeholder="000-0000-0000"></label><input type="submit" value="送信"></form>
-<form action="xxx.php" method="post"><label>レンジ（type="range"）:<input type="range" name="range"></label><input type="submit" value="送信"></form>
-<form action="xxx.php" method="post"><label>色（type="color"）:<input type="color" name="color"></label><input type="submit" value="送信"></form>
+<form action="xxx.php" method="post"> <label>メール（type="email"）:
+        <input type="email" name="email" placeholder="info@amiten.co.jp">
+      </label> <input type="submit" value="送信"> </form>
+<form action="xxx.php" method="post"> <label>検索（type="search"）:
+        <input type="search" name="search" placeholder="ワード入力">
+      </label> <input type="submit" value="送信"> </form>
+<form action="xxx.php" method="post"> <label>電話（type="telephone"）:
+        <input type="tel" name="tel" placeholder="000-0000-0000">
+      </label> <input type="submit" value="送信"> </form>
+<form action="xxx.php" method="post"> <label>レンジ（type="range"）:
+        <input type="range" name="range">
+      </label> <input type="submit" value="送信"> </form>
+<form action="xxx.php" method="post"> <label>色（type="color"）:
+        <input type="color" name="color">
+      </label> <input type="submit" value="送信"> </form>
 </section>
 </article>
