d0a36112df5d2bc276d60f27b7929c16536654d6
amishiro <amishiro@gmail.com>
2016-03-07 14:12:41 +0900

    issue try
    #37

--- public/inc/header.php
+++ public/inc/header.php
@@ -10,9 +10,8 @@
   <ul>
     <li><a href="<?php echo $link_url ?>/" class="home"><span>ホーム</span></a></li>
     <li><a href="<?php echo $link_url ?>/units" class="units"><span>units-sample</span></a></li>
-    <li><a href="#"><span>menu</span></a></li>
-    <li><a href="#"><span>menu</span></a></li>
-    <li><a href="#"><span>menu</span></a></li>
+    <li><a href="#"><span>menu2</span></a></li>
+    <li><a href="#"><span>menu3</span></a></li>
   </ul>
   </div>
 </nav>
