1d3ae0df034033a7fcbdd4618aecb5b25767743e
amishiro <amishiro@gmail.com>
2016-03-29 08:35:53 +0900

    #destファイル削除

--- dest/inc/pan.php
+++
@@ -1,7 +0,0 @@
-<div id="pan">
-<ul class="pan-nav" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-<li><a href="<?php echo $base_url ?>/" itemprop="url"><span itemprop="title">HOME</span></a></li>
-<li><a href="#" itemprop="url"><span itemprop="title">DirIndexName</span></a></li>
-<li><a href="#" itemprop="url"><span itemprop="title"><?php echo $title?></span></a></li>
-</ul>
-</div>
