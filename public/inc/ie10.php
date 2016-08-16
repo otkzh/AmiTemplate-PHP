c3eb3bb132f8a835e0b9da4f7d1601e55e705db6
amishiro <amishiro@gmail.com>
2016-08-16 10:35:05 +0900

    sass-mixin-baseを更新

--- public/inc/ie10.php
+++ public/inc/ie10.php
@@ -1,4 +1,3 @@
 <!--[if lt IE 10]>
-<div class="browsehappy"><p class="wrapper">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p></div>
+<div class="browsehappy"><p class="wrapper">お使いのブラウザはサポートされていません。当サイトをお楽しみにいただくには、<a href="http://browsehappy.com/" target="_blank" rel="nofollow">最新バージョンへのアップデート</a>をお願いいたします。</p></div>
 <![endif]-->
-
