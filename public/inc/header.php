8fba231d495cfc9c2c1aea2b4e7a19b04f55035c
amishiro <amishiro@gmail.com>
2016-08-21 00:08:16 +0900

    velocityで書き直し

--- public/inc/header.php
+++ public/inc/header.php
@@ -1,6 +1,6 @@
 <header id="header">
 
-  <div class="header-wrap wrap">
+  <div class="header-wrap">
 
     <div class="header-hgroup">
       <h1 class="header-logo"><a href="<?php echo $link_url ?>/"><?php echo $sitename ?></a></h1>
@@ -9,16 +9,16 @@
       </p>
     </div>
 
-    <nav class="header-nav js-close">
+    <nav class="header-nav">
       <ul class="header-gnav">
         <li><a href="<?php echo $base_url ?>/" class="home"><span>Home</span></a></li>
-        <li><a href="<?php echo $base_url ?>/markup" class="markup"><span>Markup</span></a></li>
-        <li><a href="<?php echo $base_url ?>/speed" class="speed"><span>Speed</span></a></li>
-        <li><a href="<?php echo $base_url ?>/responsive" class="responsive"><span>Responsive</span></a></li>
-        <li><a href="<?php echo $base_url ?>/units" class="units"><span>Units</span></a></li>
+        <li><a href="<?php echo $base_url ?>/markup/" class="markup"><span>Markup</span></a></li>
+        <li><a href="<?php echo $base_url ?>/speed/" class="speed"><span>Speed</span></a></li>
+        <li><a href="<?php echo $base_url ?>/responsive/" class="responsive"><span>Responsive</span></a></li>
+        <li><a href="<?php echo $base_url ?>/units/" class="units"><span>Units</span></a></li>
       </ul>
       <ul class="header-hnav">
-        <li><a href="<?php echo $base_url ?>/readme" class="readme"><span>Readme</span></a></li>
+        <li><a href="<?php echo $base_url ?>/readme/" class="readme"><span>Readme</span></a></li>
       </ul>
     </nav>
 
--- public/js/common.ts
+++ public/js/common.ts
@@ -1,52 +1,115 @@
+//--------------------- ページ切替時の挙動 ---------------------//
+
+//切替え準備中
+$(window).on('pjax:fetch', function () {
+
+  $('#main,#pan,#main-v').velocity({
+    opacity: 0
+  }, {
+    duration: 300,
+    queue: false
+  });
+
+});
+
+//切替えスタート
+$(document).on('pjax:ready', function () {
+  //console.timeEnd('pjax: fetch -> ready');
+  $("#main,#pan").css({
+    opacity: 0
+  });
+  $('#main').velocity({
+    opacity: 1
+  }, {
+    duration: 300,
+    queue: false
+  });
+  $('#pan').velocity({
+    opacity: 1
+  }, {
+    duration: 700,
+    queue: false
+  });
+});
+
 //--------------------- pagetop ---------------------//
 
 //use：plugins/smooth-scroll.js
 declare var smoothScroll: any;
 
-smoothScroll.init({
-  speed: 500,
-  updateURL: false,
-  offset: 100
-});
-
 //トップへ戻るがニョキッ
-$(document).on("ready pjax:ready", function(){
+$(document).on("ready pjax:ready", function () {
 
   var amount = 500;
-  var pagetop = $("#pagetop");
+  var $pagetop = $("#pagetop");
+
+  smoothScroll.init({
+    speed: 500,
+    updateURL: false,
+    offset: 100
+  });
 
   $(window).on("load scroll resize", getScrollTop);
 
   function getScrollTop() {
     var scrollSize = $(window).scrollTop();
     if (scrollSize < amount) {
-      pagetop.addClass("hide");
+      $pagetop.addClass("hide");
     } else if (scrollSize > amount) {
-      pagetop.removeClass("hide");
+      $pagetop.removeClass("hide");
     }
   };
 
 }); //トップへ戻るがニョキッ
 
+
 //--------------------- 文字数カット ---------------------//
 
 //thanks：http://black-flag.net/jquery/20121010-4227.html
 
-$(document).on("ready pjax:ready", cutText(100,'.js-cut-text'));
+$(document).on("ready pjax:ready",function(){ 
+  cutText(100, '.js-cut-text');
+  cutText(50, '.js-cut-text-small');
+});
+               
+function cutText(num: number, ele: any) {
+  var $setElm = $(ele);
+  var cutFigure = num; // カットする文字数
+  var afterTxt = ' …'; // 文字カット後に表示するテキスト
 
-$(document).on("ready pjax:ready", cutText(50,'.js-cut-text-small'));
+  $setElm.each(function () {
+    var textLength = $(this).text().length;
+    var textTrim = $(this).text().substr(0, (cutFigure));
+    if (cutFigure < textLength) {
+      $(this).html(textTrim + afterTxt);
+    };
+  });
 
-function cutText(num,ele){
-    var $setElm = $(ele);
-    var cutFigure = num; // カットする文字数
-    var afterTxt = ' …'; // 文字カット後に表示するテキスト
- 
-    $setElm.each(function(){
-        var textLength = $(this).text().length;
-        var textTrim = $(this).text().substr(0,(cutFigure))
- 
-        if(cutFigure < textLength) {
-            $(this).html(textTrim + afterTxt);
-        }
-    });
+};
+
+//--------------------- menu ---------------------//
+
+$(document).on('click', '#js-menu a', function () {
+
+  var allSlide = $(window).width() - 70;
+  $("body,#js-menu").addClass("open-menu");
+  $("#header,#main-v,#pan,#main,#footer").velocity({
+    left: -allSlide
+  },{
+    duration: 500,
+    queue: false
+  });
+  return false;
+});
+
+$(document).on('click', 'body.open-menu,#js-menu.open-menu a', function () {
+
+ $("#pan,#main-v,#main,#footer,#header").velocity({
+    left: 0
+  },{
+    duration: 500,
+    queue: false
+  });
+  $("#js-menu.open-menu").removeClass("open-menu");
+  return false;
 });
