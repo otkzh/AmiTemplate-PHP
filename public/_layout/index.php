d2ac6e52bc460fbbf5cb30f437a9ff843bf6ae42
amishiro <amishiro+github@gmail.com>
2017-11-20 14:05:23 +0900

    一時中断

--- public/_layout/index.php
+++ public/_layout/index.php
@@ -21,7 +21,7 @@ include ($inc_path."/lib/inc/header.php");
     include ($inc_path."/lib/inc/pan.php");
     ?>
     <section class="main__item">
-      <h2 class="u-ttl_l"><a href="./_full/index.php" class="u-icon__link">親要素から子要素をはみ出して表示</a></h2>
+      <h2 class="u-ttl_l"><a href="<?php echo $base_url; ?>/_styleguide/" class="u-icon__link">親要素から子要素をはみ出して表示</a></h2>
       <p>.l-full</p>
     </section>
   </article>
