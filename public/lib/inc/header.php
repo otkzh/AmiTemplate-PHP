ef02650480ffa6d9398710ea12fef729e1276944
amishiro <amishiro+github@gmail.com>
2017-11-25 23:38:39 +0900

    マークアップ＆scssブラッシュアップ #126

--- public/lib/inc/header.php
+++ public/lib/inc/header.php
@@ -6,17 +6,17 @@
     </h1>
     <nav class="header__gnav gnav">
       <ul class="gnav__row">
-        <li class="gnav__item_home">
-          <a href="<?php echo $base_url ?>/" class="gnav__link">home</a>
+        <li class="gnav__item">
+          <a href="<?php echo $base_url ?>/" class="gnav__link_home">home</a>
         </li>
-        <li class="gnav__item_sample">
-          <a href="<?php echo $base_url ?>/_sample/" class="gnav__link">sample</a>
+        <li class="gnav__item">
+          <a href="<?php echo $base_url ?>/_sample/" class="gnav__link_sample">sample</a>
         </li>
-        <li class="gnav__item_layout">
-          <a href="<?php echo $base_url ?>/_layout/" class="gnav__link">layout</a>
+        <li class="gnav__item">
+          <a href="<?php echo $base_url ?>/_layout/" class="gnav__link_layout">layout</a>
         </li>
-        <li class="gnav__item_styleguide gnav-deep">
-          <label class="gnav__link">styleguide</label>
+        <li class="gnav__item gnav-deep">
+          <label class="gnav__link_styleguide">styleguide</label>
 
           <nav class="gnav-deep__nav">
             <ul class="gnav-deep__row">
@@ -51,8 +51,8 @@
           </nav>
 
         </li>
-        <li class="gnav__item_contact">
-          <a href="#" class="gnav__link">contact</a>
+        <li class="gnav__item">
+          <a href="#" class="gnav__link_contact">contact</a>
         </li>
       </ul>
     </nav>
