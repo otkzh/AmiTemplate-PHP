0f90317ec6fdd05be32c0572f21921a7c67039f1
amishiro <amishiro+github@gmail.com>
2017-11-30 09:52:48 +0900

    表示時に複数でも発火 #124

--- public/lib/_es2015/base/_intersection-observer/__config.js
+++ public/lib/_es2015/base/_intersection-observer/__config.js
@@ -1,31 +1,38 @@
 export default function () {
 
+  //監視対象のセレクター
   const targets = document.querySelectorAll('.interso-target');
 
-  // オプションを指定する。
-  const options = {
-    //root: document.querySelector('#all'),
-    rootMargin: '-20%',　//root（通常はviewpoert）のmarginをコントロール
-    //threshold: [0,　0.5, 1.0]
-  }
+  //セレクターの数だけ実行
+  for (let el of targets){
+    observerFn(el);
+  };
 
-  // オプションとともにIntersectionObserverオブジェクトを作成する。
-  let observer = new IntersectionObserver((entry) => {
+  //実行内容
+  function observerFn(target){
 
-    let e = entry[0];
-    // console.log(e);
-
-    if(e.isIntersecting == true){
-      $(e.target).html(e.target.className + '入ったよ（監視中）');
-    }else{
-      $(e.target).html(e.target.className + '出たよ（監視中）');
+    // オプションを指定する。
+    const options = {
+      //root: document.querySelector('#all'),
+      rootMargin: '-20%',　//root（通常はviewpoert）のmarginをコントロール
+      threshold: [0, 0.5, 1] //発火のタイミング(0=bttom,1=top)
     }
 
-  }, options);
+    // オプションとともにIntersectionObserverオブジェクトを作成する。
+    const observer = new IntersectionObserver((entry) => {
 
-  // 監視したい要素をobserveする。例：observer.observe(target);
-  for (let el of targets){
-    observer.observe(el);
-  }
+      let e = entry[0];
+
+      if(e.isIntersecting == true){
+        e.target.innerHTML = '入ってるよ（監視中）';
+      }else{
+        e.target.innerHTML = '出てるよ（監視中）';
+      }
+
+    }, options);
+
+    // 監視したい要素をobserve
+    observer.observe(target);
+  };
 
 };
